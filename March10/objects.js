78630db5edaeebd7e9c3d4b33c7784b967d8262b
Ray Merrell <RayMerrell@outlook.com>
2023-03-10 11:31:37 +0000

    M46Course

---
+++ March10/objects.js
@@ -0,0 +1,18 @@
+// const objCafe = {
+//     _strName : "Whitesheep",
+//     _intSeatingCapacity: 100;
+//     _booHasSpecialOffers: true,
+//     _arrDrinks: [
+//         "Tea",
+//         "Latte",
+//         "Filter Coffee",
+//         "Hot Chocolate"
+//     ]
+// };
+
+const objPerson = {
+    _strName : "Ray",
+    _intAge :"55"
+};
+
+console.log(objPerson._strName + " is " + objPerson._intAge + " Years old");
